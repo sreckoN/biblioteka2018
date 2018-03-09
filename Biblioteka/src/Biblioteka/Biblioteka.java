46c0979ea8cc71adf7d64a9fc11b0da86a20eb9c
sreckoN <srecko.nikolic71@hotmail.com>
2018-03-09 15:02:06 +0100

    Moj javni komentar

--- Biblioteka/src/Biblioteka/Biblioteka.java
+++ Biblioteka/src/Biblioteka/Biblioteka.java
@@ -27,6 +27,7 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, Autor autor, String isbn, String izdavac) {
+		//Komentar javni
 		if(naslov == null) throw new RuntimeException("Morate uneti naslov");
 		
 		LinkedList<Knjiga> novaLista = new LinkedList<Knjiga>();
